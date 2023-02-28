9eac964c6df90c6a33edb37cc1f248da4443a147
Denise <deniselanzieri@gmail.com>
2023-02-28 11:36:23 +0100

    format yapf 1

--- sbi_lens/simulator/LogNormal_field.py
+++ sbi_lens/simulator/LogNormal_field.py
@@ -21,7 +21,7 @@ lognormal_params = np.loadtxt(DATA_DIR / "lognormal_shift.csv",
 
 @jax.jit
 def shift_fn(omega_m, sigma_8):
-    """
+  """
   Compute the interpolated shift parameters of the lognormal distribution as function of cosmology
   -----------
   omega_m: float
@@ -36,20 +36,20 @@ def shift_fn(omega_m, sigma_8):
   Interpolated shift parameters
 
   """
-    omega_m = jnp.atleast_1d(omega_m)
-    sigma_8 = jnp.atleast_1d(sigma_8)
-    lambda_shift = map_coordinates(lognormal_params[:, :, 2],
-                                   jnp.stack([(omega_m - 0.2) / 0.2 * 8 - 0.5,
-                                              (sigma_8 - 0.6) / 0.4 * 8 - 0.5],
-                                             axis=0).reshape([2, -1]),
-                                   order=1,
-                                   mode='nearest').squeeze()
+  omega_m = jnp.atleast_1d(omega_m)
+  sigma_8 = jnp.atleast_1d(sigma_8)
+  lambda_shift = map_coordinates(lognormal_params[:, :, 2],
+                                 jnp.stack([(omega_m - 0.2) / 0.2 * 8 - 0.5,
+                                            (sigma_8 - 0.6) / 0.4 * 8 - 0.5],
+                                           axis=0).reshape([2, -1]),
+                                 order=1,
+                                 mode='nearest').squeeze()
 
-    return lambda_shift
+  return lambda_shift
 
 
 def make_power_map(pk_fn, N, map_size, zero_freq_val=0.0):
-    """
+  """
   Calculate fourier-space Gaussian fields generated with a given power spectrum.
   -----------
   pk_fn:
@@ -70,17 +70,17 @@ def make_power_map(pk_fn, N, map_size, zero_freq_val=0.0):
   Image
 
   """
-    k = 2 * jnp.pi * jnp.fft.fftfreq(N, d=map_size / N)
-    kcoords = jnp.meshgrid(k, k)
-    k = jnp.sqrt(kcoords[0]**2 + kcoords[1]**2)
-    ps_map = pk_fn(k)
-    ps_map = ps_map.at[0, 0].set(zero_freq_val)
-    power_map = ps_map * (N / map_size)**2
-    return power_map
+  k = 2 * jnp.pi * jnp.fft.fftfreq(N, d=map_size / N)
+  kcoords = jnp.meshgrid(k, k)
+  k = jnp.sqrt(kcoords[0]**2 + kcoords[1]**2)
+  ps_map = pk_fn(k)
+  ps_map = ps_map.at[0, 0].set(zero_freq_val)
+  power_map = ps_map * (N / map_size)**2
+  return power_map
 
 
 def make_lognormal_power_map(power_map, shift, zero_freq_val=0.0):
-    """
+  """
   Calculate Log-Normal lensing fields with given Gaussian fields.
   -----------
   power_map: Jax.DeviceArray
@@ -97,13 +97,13 @@ def make_lognormal_power_map(power_map, shift, zero_freq_val=0.0):
   power_spectrum_for_lognorm: Jax.DeviceArray
   Log-Normal lensing fields
   """
-    power_spectrum_for_lognorm = jnp.fft.ifft2(power_map).real
-    power_spectrum_for_lognorm = jnp.log(1 +
-                                         power_spectrum_for_lognorm / shift**2)
-    power_spectrum_for_lognorm = jnp.abs(
-        jnp.fft.fft2(power_spectrum_for_lognorm))
-    power_spectrum_for_lognorm = power_spectrum_for_lognorm.at[0, 0].set(0.)
-    return power_spectrum_for_lognorm
+  power_spectrum_for_lognorm = jnp.fft.ifft2(power_map).real
+  power_spectrum_for_lognorm = jnp.log(1 +
+                                       power_spectrum_for_lognorm / shift**2)
+  power_spectrum_for_lognorm = jnp.abs(
+      jnp.fft.fft2(power_spectrum_for_lognorm))
+  power_spectrum_for_lognorm = power_spectrum_for_lognorm.at[0, 0].set(0.)
+  return power_spectrum_for_lognorm
 
 
 def lensingLogNormal(N=128,
@@ -112,7 +112,7 @@ def lensingLogNormal(N=128,
                      sigma_e=0.26,
                      model_type='lognormal',
                      with_noise=True):
-    """
+  """
   Calculate Log-Normal lensing convergence map.
   -----------
   N: int
@@ -140,38 +140,37 @@ def lensingLogNormal(N=128,
   Lensing convergence map
   """
 
-    pix_area = (map_size * 60 / N)**2
-    map_size = map_size / 180 * jnp.pi
-    omega_c = numpyro.sample('omega_c', dist.Normal(0.3, 0.05))
-    sigma_8 = numpyro.sample('sigma_8', dist.Normal(0.8, 0.05))
-    cosmo = jc.Planck15(Omega_c=omega_c, sigma8=sigma_8)
-    pz = jc.redshift.smail_nz(0.5, 2., 1.0)
-    tracer = jc.probes.WeakLensing([pz])
-    ell_tab = jnp.logspace(0, 4.5, 128)
-    cell_tab = jc.angular_cl.angular_cl(cosmo, ell_tab, [tracer])[0]
-    P = lambda k: jc.scipy.interpolate.interp(k.flatten(), ell_tab, cell_tab
-                                              ).reshape(k.shape)
-    z = numpyro.sample(
-        'z',
-        dist.MultivariateNormal(loc=jnp.zeros((N, N)),
-                                precision_matrix=jnp.eye(N)))
+  pix_area = (map_size * 60 / N)**2
+  map_size = map_size / 180 * jnp.pi
+  omega_c = numpyro.sample('omega_c', dist.Normal(0.3, 0.05))
+  sigma_8 = numpyro.sample('sigma_8', dist.Normal(0.8, 0.05))
+  cosmo = jc.Planck15(Omega_c=omega_c, sigma8=sigma_8)
+  pz = jc.redshift.smail_nz(0.5, 2., 1.0)
+  tracer = jc.probes.WeakLensing([pz])
+  ell_tab = jnp.logspace(0, 4.5, 128)
+  cell_tab = jc.angular_cl.angular_cl(cosmo, ell_tab, [tracer])[0]
+  P = lambda k: jc.scipy.interpolate.interp(k.flatten(), ell_tab, cell_tab
+                                            ).reshape(k.shape)
+  z = numpyro.sample(
+      'z',
+      dist.MultivariateNormal(loc=jnp.zeros((N, N)),
+                              precision_matrix=jnp.eye(N)))
 
-    power_map = make_power_map(P, N, map_size)
-    if model_type == 'lognormal':
-        shift = shift_fn(cosmo.Omega_m, sigma_8)
-        power_map = make_lognormal_power_map(power_map, shift)
-    field = jnp.fft.ifft2(jnp.fft.fft2(z) * jnp.sqrt(power_map)).real
-    if model_type == 'lognormal':
-        field = shift * (jnp.exp(field - jnp.var(field) / 2) - 1)
+  power_map = make_power_map(P, N, map_size)
+  if model_type == 'lognormal':
+    shift = shift_fn(cosmo.Omega_m, sigma_8)
+    power_map = make_lognormal_power_map(power_map, shift)
+  field = jnp.fft.ifft2(jnp.fft.fft2(z) * jnp.sqrt(power_map)).real
+  if model_type == 'lognormal':
+    field = shift * (jnp.exp(field - jnp.var(field) / 2) - 1)
 
-    if with_noise == True:
-        x = numpyro.sample(
-            'y',
-            dist.Independent(
-                dist.Normal(field,
-                            sigma_e / jnp.sqrt(gal_per_arcmin2 * pix_area)),
-                2))
-    else:
-        x = numpyro.deterministic('y', field)
+  if with_noise == True:
+    x = numpyro.sample(
+        'y',
+        dist.Independent(
+            dist.Normal(field, sigma_e / jnp.sqrt(gal_per_arcmin2 * pix_area)),
+            2))
+  else:
+    x = numpyro.deterministic('y', field)
 
-    return x
+  return x
--- tests/test_bijector.py
+++ tests/test_bijector.py
@@ -31,50 +31,48 @@ _test_params_MASB = [{
 
 
 def test_bijectorMixtureAffineSigmoidBijector_inverse():
-    """
+  """
     Testing that the inverse transform of MixtureAffineSigmoidBijector
     is indeed the inverse of forward.
     """
 
-    batch_size = 100
+  batch_size = 100
 
-    for params in _test_params_MASB:
-        x = jax.random.uniform(jax.random.PRNGKey(0),
-                               shape=[batch_size, params['nb_dimension']])
+  for params in _test_params_MASB:
+    x = jax.random.uniform(jax.random.PRNGKey(0),
+                           shape=[batch_size, params['nb_dimension']])
 
-        # shape for components of the transform
-        shape = [batch_size, params['nb_dimension'], params['nb_component']]
-        bij = MixtureAffineSigmoidBijector(
-            jnp.ones(shape) * params['a'],
-            jnp.ones(shape) * params['b'],
-            jnp.ones(shape) * params['c'],
-            jax.nn.softmax(
-                jax.random.uniform(jax.random.PRNGKey(1), shape=shape)))
+    # shape for components of the transform
+    shape = [batch_size, params['nb_dimension'], params['nb_component']]
+    bij = MixtureAffineSigmoidBijector(
+        jnp.ones(shape) * params['a'],
+        jnp.ones(shape) * params['b'],
+        jnp.ones(shape) * params['c'],
+        jax.nn.softmax(jax.random.uniform(jax.random.PRNGKey(1), shape=shape)))
 
-    assert_allclose(bij.inverse(bij.forward(x) * 1.), x, rtol=2e-2, atol=1e-5)
+  assert_allclose(bij.inverse(bij.forward(x) * 1.), x, rtol=2e-2, atol=1e-5)
 
 
 def test_bijectorMixtureAffineSigmoidBijector_fldj_dim():
-    """
+  """
     Testing forward_log_det_jacobian dimension of MixtureAffineSigmoidBijector
     """
-    batch_size = 100
+  batch_size = 100
 
-    for params in _test_params_MASB:
-        x = jax.random.uniform(jax.random.PRNGKey(0),
-                               shape=[batch_size, params['nb_dimension']])
+  for params in _test_params_MASB:
+    x = jax.random.uniform(jax.random.PRNGKey(0),
+                           shape=[batch_size, params['nb_dimension']])
 
-        # shape for components of the transform
-        shape = [batch_size, params['nb_dimension'], params['nb_component']]
+    # shape for components of the transform
+    shape = [batch_size, params['nb_dimension'], params['nb_component']]
 
-        bij = MixtureAffineSigmoidBijector(
-            jnp.ones(shape) * params['a'],
-            jnp.ones(shape) * params['b'],
-            jnp.ones(shape) * params['c'],
-            jax.nn.softmax(
-                jax.random.uniform(jax.random.PRNGKey(1), shape=shape)))
+    bij = MixtureAffineSigmoidBijector(
+        jnp.ones(shape) * params['a'],
+        jnp.ones(shape) * params['b'],
+        jnp.ones(shape) * params['c'],
+        jax.nn.softmax(jax.random.uniform(jax.random.PRNGKey(1), shape=shape)))
 
-    assert_equal(
-        bij.forward_log_det_jacobian(x * 1).shape,
-        (batch_size, params['nb_dimension']),
-        'forward_log_det_jacobian output dim is not (batch_size,nb_dimension)')
+  assert_equal(
+      bij.forward_log_det_jacobian(x * 1).shape,
+      (batch_size, params['nb_dimension']),
+      'forward_log_det_jacobian output dim is not (batch_size,nb_dimension)')
